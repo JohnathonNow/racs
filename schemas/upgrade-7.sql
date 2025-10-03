035e2b32696996e789d0a47fb0a50b3d24339eae
Raja Mukherji <rajamukherji@gmail.com>
2025-10-03 09:41:50 +0100

    Updates.

--- schemas/upgrade-7.sql
+++ schemas/upgrade-7.sql
@@ -2,4 +2,4 @@ ALTER TABLE credentials ADD COLUMN updated INTEGER;
 ALTER TABLE credentials ADD COLUMN name STRING;
 UPDATE credentials SET name = description;
 UPDATE credentials SET updated = 0 WHERE updated IS NULL;
-UPDATE credentials SET expired = 0 WHERE expired IS NULL;
+UPDATE credentials SET expiry = 0 WHERE expired IS NULL;
